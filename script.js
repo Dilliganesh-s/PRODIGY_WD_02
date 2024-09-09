fdb82c0066b26da3d3b1e9438c30dc56662d7a0b
Dilliganesh-s <146836855+Dilliganesh-s@users.noreply.github.com>
2024-09-09 21:24:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+let seconds = 0;
+let tens = 0;
+let mins = 0;
+let getSeconds = document.querySelector('.seconds');
+let getTens = document.querySelector('.tens');
+let getMins = document.querySelector('.mins');
+let btnStart = document.querySelector('.btn-start');
+let btnStop = document.querySelector('.btn-stop');
+let btnReset = document.querySelector('.btn-reset');
+let btnLap = document.querySelector('.btn-lap');
+let lapList = document.getElementById('lap-list');
+let interval;
+let lapCount = 0;
+
+btnStart.addEventListener('click', () => {
+    clearInterval(interval);
+    interval = setInterval(startTimer, 10);
+});
+
+btnStop.addEventListener('click', () => {
+    clearInterval(interval);
+});
+
+btnReset.addEventListener('click', () => {
+    clearInterval(interval);
+    tens = '00';
+    seconds = '00';
+    mins = '00';
+    getSeconds.innerHTML = seconds;
+    getTens.innerHTML = tens;
+    getMins.innerHTML = mins;
+    lapList.innerHTML = '';
+    lapCount = 0;
+});
+
+btnLap.addEventListener('click', () => {
+    if (interval) {
+        lapCount++;
+        let lapTime = `${getMins.innerHTML}:${getSeconds.innerHTML}:${getTens.innerHTML}`;
+        let lapItem = document.createElement('li');
+        lapItem.textContent = `Lap ${lapCount}: ${lapTime}`;
+        lapList.appendChild(lapItem);
+    }
+});
+
+function startTimer() {
+    tens++;
+    if (tens <= 9) {
+        getTens.innerHTML = '0' + tens;
+    }
+    if (tens > 9) {
+        getTens.innerHTML = tens;
+    }
+    if (tens > 99) {
+        seconds++;
+        getSeconds.innerHTML = '0' + seconds;
+        tens = 0;
+        getTens.innerHTML = '0' + 0;
+    }
+    if (seconds > 9) {
+        getSeconds.innerHTML = seconds;
+    }
+    if (seconds > 59) {
+        mins++;
+        getMins.innerHTML = '0' + mins;
+        seconds = 0;
+        getSeconds.innerHTML = '0' + 0;
+    }
+    if (mins > 9) {
+        getMins.innerHTML = mins;
+    }
+}
